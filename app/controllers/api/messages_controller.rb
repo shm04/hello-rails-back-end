04471206272c6d1ca94a38358e38363cb2cc7ec8
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-12-14 13:01:26 -0600

    Add linters

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,18 +1,17 @@
 class Api::MessagesController < ApplicationController
-
   before_action :set_cors_headers
 
-    def index
-      @messages = Message.all
-      render json: @messages
-    end
-
-    private
-
-    def set_cors_headers
-      response.headers['Access-Control-Allow-Origin'] = '*'
-      response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD'
-      response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-      response.headers['Access-Control-Max-Age'] = '1728000'
-    end
+  def index
+    @messages = Message.all
+    render json: @messages
   end
+
+  private
+
+  def set_cors_headers
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Max-Age'] = '1728000'
+  end
+end
